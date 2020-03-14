11b1ddf3b590d8dc5c03223ef3a85335ecd4c8a9
Arthur-St <artrexwow@gmail.com>
2020-03-14 19:57:21 -0300

    Rebith
    Rebirth

---
+++ projeto 1/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Rigidbody2D PlayerTransform; // Player Transform
+	public Transform CameraTransform; // Camera Transform
+	//public GameObject PlayerObject; // Capturar obj Do player
+
+
+	// Use this for initialization
+	void Start()
+	{
+		PlayerTransform = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
+		CameraTransform = GetComponent<Transform>();
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		CameraTransform.position = 
+			Vector3.Lerp(CameraTransform.position,
+			new Vector3(PlayerTransform.position.x, PlayerTransform.position.y, CameraTransform.position.z),2*Time.deltaTime);
+
+
+	}
+}
---
+++ projeto 1/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 10.0f, JumpForce = 10.0f;
+    public LayerMask GroundLayer;
+    private Rigidbody2D rb;
+    //public BoxCollider2D BcPlayer;
+    public bool teste;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //BcPlayer = GetComponentInChildren<BoxCollider2D>();
+    }
+    //  ##  ##  ##  ##  ##  ##  ## //
+    void Update()
+    {
+        float translation = Input.GetAxis("Horizontal") * speed;
+        rb.velocity = new Vector2(translation, rb.velocity.y);
+        if (Input.GetKeyDown("space") && fIsground())
+            Jump();
+        teste = fIsground();
+
+
+    }
+    void Jump()
+    {
+        rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+    }
+    private bool fIsground()
+    {
+        return Physics2D.BoxCast(new Vector2(transform.localPosition.x, transform.localPosition.y), new Vector2(transform.localScale.x * 0.5f, transform.localScale.y*0.9f), 0, Vector2.down,0.1f, GroundLayer);  
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(new Vector3(transform.localPosition.x, transform.localPosition.y, 0), new Vector3(transform.localScale.x*0.5f, transform.localScale.y* 1.01f));
+    }
+}
