03902bfb43ae284fa473ef4b5e5034098f8b8f24
Arthur-St <artrexwow@gmail.com>
2020-03-14 19:37:35 -0300

    comit
    comit

--- Projeto 1/Assets/Scripts/Player.cs
+++ Projeto 1/Assets/Scripts/Player.cs
@@ -21,26 +21,24 @@ public class Player : MonoBehaviour
     {
         float translation = Input.GetAxis("Horizontal") * speed;
         rb.velocity = new Vector2(translation, rb.velocity.y);
-        teste=fIsground();
         if (Input.GetKeyDown("space") && fIsground())
             Jump();
-        else
-            rb.gravityScale += 2*Time.deltaTime;
+        teste = fIsground();
+
+
     }
     void Jump()
     {
         rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
-        rb.gravityScale = 1;
-
     }
     private bool fIsground()
     {
-        return Physics2D.BoxCast(new Vector2(transform.localPosition.x, transform.localPosition.y - 0.4f), new Vector2(0.5f, 0.2f), 0, Vector2.down,0.2f, GroundLayer);  
+        return Physics2D.BoxCast(new Vector2(transform.localPosition.x, transform.localPosition.y), new Vector2(transform.localScale.x * 0.5f, transform.localScale.y * 1.01f), 0, Vector2.down,0.2f, GroundLayer);  
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(new Vector3(transform.localPosition.x, transform.localPosition.y-0.4f, 0), new Vector3(0.5f,0.2f));
+        Gizmos.DrawWireCube(new Vector3(transform.localPosition.x, transform.localPosition.y, 0), new Vector3(transform.localScale.x*0.5f, transform.localScale.y* 1.01f));
     }
 }
